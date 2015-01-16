44f061d852e57e5aa15a673f4a0a070d64ea848a
berickson926 <berickson926@gmail.com>
2015-01-15 18:11:15 -0800

    Basic App setup
    libgdx setup
    additional libs and dependencies
    core app logic

---
+++ Main/src/com/enplug/photolist/PhotoList.java
@@ -0,0 +1,46 @@
+package com.enplug.photolist;
+
+import com.enplug.sdk.hosting.HostedGame;
+import com.enplug.sdk.interfaces.IServiceProvider;
+import com.enplug.sdk.model.social.SocialFeedDefinition;
+
+import java.util.List;
+
+public class PhotoList extends HostedGame
+{
+    @Override
+    public void initialize(IServiceProvider serviceProvider, List<SocialFeedDefinition> feeds, boolean isLandscape, String language)
+    {
+
+    }
+
+    @Override
+    public void resume()
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void create()
+    {
+
+    }
+
+    @Override
+    public int getVersion()
+    {
+        return 0;
+    }
+
+    @Override
+    public void dispose()
+    {
+
+    }
+}
