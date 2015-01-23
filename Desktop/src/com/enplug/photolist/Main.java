e6840cf24e99ba8f4af0efb7610dd516d1aa7f1d
berickson926 <berickson926@gmail.com>
2015-01-22 17:44:52 -0800

    adding in libs

--- Desktop/src/com/enplug/photolist/Main.java
+++ Desktop/src/com/enplug/photolist/Main.java
@@ -3,14 +3,15 @@ package com.enplug.photolist;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
-	public static void main(String[] args) {
+public class Main
+{
+	public static void main(String[] args)
+    {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "PhotoList";
-		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
-		
+
 		new LwjglApplication(new PhotoList(), cfg);
 	}
 }
