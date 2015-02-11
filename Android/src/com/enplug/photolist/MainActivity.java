8db7464d3e3e02d4d47b638894ee409a740ef707
berickson926 <berickson926@gmail.com>
2015-02-10 20:35:06 -0800

    android lib fixes

--- Android/src/com/enplug/photolist/MainActivity.java
+++ Android/src/com/enplug/photolist/MainActivity.java
@@ -3,6 +3,8 @@ package com.enplug.photolist;
 import android.app.ActivityManager;
 import android.os.Bundle;
 
+import android.util.Log;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.enplug.test.AndroidTestContext;
